d0b91146398f338048e4575c6f1a8e23b840f5de
BradLFowler <BradLF00@gmail.com>
2022-05-05 18:33:36 -0500

    added getEmployeesByFirstName

--- controllers/employees.js
+++ controllers/employees.js
@@ -11,6 +11,15 @@
 // Create a function in the controller called "getEmployeesById". It should be called by the "/:id" route. For now, leave the logic the same: res.send("getting employees...")
 
 
+// Create a function called "getEmployeesByFirstName. It should select all fields from the employees table where the first_name matches the /:first_name query parameter"
+
+const getEmployeesByFirstName = (req, res) => {
+    pool.query("SELECT * FROM employees WHERE first_name = req.params.first_name", (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
+}
+
 // module.exports = {
 
 // };
