887fd39976f43eada9e7e6c340026b05e2803b8d
BradLFowler <BradLF00@gmail.com>
2022-05-05 20:38:44 -0500

    should be ready for testing

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 
 const employeesRouter = require('./routers/employees');
+const departmentsRouter = require('./routes/departments');
+const salariesRouter = require('./routes/salaries');
+const titlesRouter = require('./routes/titles');
+
 const port = process.env.PORT || 4001;
 
 
 app.use(employeesRouter);
+app.use(departmentsRouter);
+app.use(salariesRouter);
+app.use(titlesRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to our API!')
